a81108a711820fdb0d453bd706cffee78ec0f17a
rmetel <ralph.metel@gmx.de>
2022-04-21 03:38:43 +0200

    added country dropDown

--- src/main/frontend/src/component/WineList.js
+++ src/main/frontend/src/component/WineList.js
@@ -3,34 +3,48 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import {
+  Badge,
+  Button,
   Card,
   Container,
-  Button,
-  Badge
+  DropdownButton,
+  Dropdown,
 } from "react-bootstrap";
 
 class WineList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {wines: []};
+    this.state = {wines: [], countries: []};
   }
 
   componentDidMount() {
     let isLocal = window.location.href.indexOf("localhost") > -1;
     let urlLocal = "http://localhost:8080";
-    let endPoint = "/wine/all";
+    let endPointWines = "/wine/all";
+    let endPointCountries = "/country/all";
 
-    fetch(isLocal? urlLocal + endPoint : endPoint)
+    fetch(isLocal? urlLocal + endPointWines : endPointWines)
       .then(response => response.json())
       .then(data => this.setState({wines: data}));
+
+    fetch(isLocal? urlLocal + endPointCountries : endPointCountries)
+      .then(response => response.json())
+      .then(data => this.setState({countries: data}));
   }
 
   render() {
     const {wines} = this.state;
+    const {countries} = this.state;
     return (
       <>
         <Container className="mt-5">
           <div className="row">
+            <div className="col-12 col-sm-12 mb-4 d-flex flex-row-reverse">
+              <DropdownButton id="dropdown-basic-button" title="Länder" variant="secondary">
+                {countries.map(country => <Dropdown.Item key={country.id}>{country.name}</Dropdown.Item>)}
+              </DropdownButton>
+            </div>
+
             {wines.map(wine =>
               <div className="col-12 col-sm-3 mb-2" key={wine.id}>
                 <Card>
